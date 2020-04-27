6901ea4948b0c03e5e03cbe83c6e1dcfeddc1aab
Lyle <LyleLengyel@gmail.com>
2020-04-27 15:50:16 -0700

    proj4

---
+++ chapter01/proj4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float amount;
+
+	printf("Enter a dollars-and-cents amount: $");
+	scanf("%f", &amount);
+
+	printf("With tax added: %.2f\n", 1.05*amount);
+	return 0;
+}
