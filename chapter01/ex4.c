6c61699fe71e22cfc73e132bc98678dc6e8b0de5
Lyle <LyleLengyel@gmail.com>
2020-04-27 13:13:12 -0700

    Complete chapter 1 exercises

---
+++ chapter01/ex4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int fruit, meat;
+	float grain, vegetable;
+
+	printf("int fruit: %d\n", fruit);
+	printf("int meat: %d\n", meat);
+	printf("float grain: %f\n", grain);
+	printf("float vegetable: %f\n", vegetable);
+
+	return 0;
+}
+
