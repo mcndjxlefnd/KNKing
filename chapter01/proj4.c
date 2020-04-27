470807836c32b5b92c5d0622b72738241158ae63
Lyle <LyleLengyel@gmail.com>
2020-04-27 15:52:49 -0700

    add '$' to proj4 output

--- chapter01/proj4.c
+++ chapter01/proj4.c
@@ -7,6 +7,6 @@ int main(void)
 	printf("Enter a dollars-and-cents amount: $");
 	scanf("%f", &amount);
 
-	printf("With tax added: %.2f\n", 1.05*amount);
+	printf("With tax added: $%.2f\n", 1.05*amount);
 	return 0;
 }
