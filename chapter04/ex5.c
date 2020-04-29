c9cec0b585c548cef883e81e073276e3d41d754a
Lyle <LyleLengyel@gmail.com>
2020-04-29 14:44:32 -0700

    complete ex5 and rename ex3, ex4 executables to reflect C standard compiled under

---
+++ chapter04/ex5.c
@@ -0,0 +1,23 @@
+/*What is the value of each of the following expressions in C89? (Give all possible values if an expression may have more than one value.)*/
+#include <stdio.h>
+
+int main(void)
+{
+/*	a) 8 % 5
+		3	*/
+	printf("a) %d\n", 8%5);
+
+/*	b) -8 % 5
+		-3	*/
+	printf("b) %d\n", -8%5);
+
+/* c) 8 % -5
+		-3 actually +3 */
+	printf("c) %d\n", 8%-5);
+
+/* d) -8 % -5
+		3	*/
+	printf("d) %d\n", -8%-5);
+
+	return 0;
+}
