c976cf4f79024e01afd8e997c6beb7a5bfc12954
Lyle <LyleLengyel@gmail.com>
2020-04-29 14:28:21 -0700

    complete ex4. rewrite and compile ex3 for C89

--- chapter04/ex3.c
+++ chapter04/ex3.c
@@ -1,25 +1,26 @@
 /*
 What is the value of each of the following expressions in C89? (Give all possible values if an
 expression may have more than one value.)
-
-	a) 8 / 5
-		1
-
-	b) -8 / 5
-		-1 or -2
-
-	c) 8 / -5
-		-1 or -2
-
-	d) -8 / -5
-		1 or 2
 */
-
 #include <stdio.h>
 
 int main(void)
 {
-	printf("%d\n", -8 / -5);
+/*	a) 8 / 5
+	1	*/
+	printf("a) %d\n", 8/5);
+
+/*	b) -8 / 5
+	-1 or -2	*/
+	printf("b) %d\n", -8/5);
+
+/*	c) 8 / -5
+	-1 or -2	*/
+	printf("c) %d\n", 8/-5);
+
+/*	d) -8 / -5
+	1 or 2	*/
+	printf("d) %d\n", -8/-5);
 
 	return 0;
 }
---
+++ chapter04/ex4.c
@@ -0,0 +1,26 @@
+/*
+What is the value of each of the following expressions in C99? (Give all possible values if an
+expression may have more than one value.)
+*/
+#include <stdio.h>
+
+int main(void)
+{
+//	a) 8 / 5
+	//	1
+	printf("a) %d\n", 8 / 5);
+
+//	b) -8 / 5
+	//	-1
+	printf("b) %d\n", -8 / 5);
+
+//	c) 8 / -5
+	//	-1
+	printf("c) %d\n", 8 / -5);
+
+//	d) -8 / -5
+	//	1
+	printf("d) %d\n", -8 / -5);
+
+	return 0;
+}
